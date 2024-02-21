09072de78e1dd974934b18db3602671c7871e5b2
Jevinalys <jevinalyskhamasi@gmail.com>
2024-02-21 13:01:35 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+location = input("And where are you located please: ")
+
+print ("Hi {} you are {} years old and you come from {}".format(name,age,location))
